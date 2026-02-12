d474123157ad61774484350cb34a460b6a58a725
AlenaSkornyakova <alenaolegovna13@gmail.com>
2026-02-12 21:38:54 +0100

    fix(vercel)

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,12 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import express from 'express';
 import { setupApp } from '../src/set-app';
 
-const app = express();
-setupApp(app);
+ const app = express();
+  setupApp(app);
 
-export default app;
+export default function handler(req: VercelRequest, res: VercelResponse) {
+ 
+  // Прокидываем запрос в Express-приложение
+  app(req as any, res as any);
+}
